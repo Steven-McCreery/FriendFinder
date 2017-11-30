f283512c5fa6109c370804d782b285320a694896
Steven McCreery <logad8@gmail.com>
2017-11-30 03:20:27 -0500

    added in html coding, custom select dropdowns not working as anticipated, starting switch to radio buttons, potential code for checking answer status and activating submit button in the works

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,38 @@
 
+// friends data is stored as an array of objects
 var friendArray = [
+	{
+		name: "Greg",
+		photo: "url",
+		scores: [
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+		]
+	},
+	{
+		name: "Sue",
+		photo: "url",
+		scores: [
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2,
+			2
+		]
+	},
 	{
 		name: "Bob",
 		photo: "url",
@@ -15,7 +48,40 @@ var friendArray = [
 			3,
 			3
 		]
-	}
+	},
+	{
+		name: "Jane",
+		photo: "url",
+		scores: [
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4,
+			4
+		]
+	},
+	{
+		name: "Igor",
+		photo: "url",
+		scores: [
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5
+		]
+	},
+
 ]
 
 module.exports = friendArray;
